ced784ce339e67f23c2828b4932fa3d562b3a158
Nishanth Vasudeva <nishanth.terikerevasudeva@mavs.uta.edu>
2016-05-27 12:10:06 -0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
 $mysql_hostname = "omega.uta.edu";
-$mysql_user = "sxs9203";
-$mysql_password = "md79fY75";
-$mysql_database = "sxs9203";
+$mysql_user = "***";
+$mysql_password = "***";
+$mysql_database = "***";
 $prefix = "";
 $bd = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Could not connect to the server");
 mysql_select_db($mysql_database, $bd) or die("Could not select database");
